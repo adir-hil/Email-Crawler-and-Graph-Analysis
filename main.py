de3db1c99451f0cd1c1a9ff1641c8fc69822996e
Adir Hilvert <75641817+adir-hil@users.noreply.github.com>
2022-07-17 00:56:37 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from crawler import EmailSpider
-
-seed = ['https://www.example.com/']
-crawl_output_filename = 'scraping_data'
-crawl = EmailSpider(seed=seed, time_limit=300, scraped_data_filename=crawl_output_filename, max_pages_to_parse=1000,
-                    rotate=False, force_exit=True)
-crawl.run_crawler()
-
